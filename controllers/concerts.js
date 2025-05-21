71c8c885d98ee7edd447a9b39d6005ec1ed2fae3
Morgan <morganridgef@gmail.com>
2025-05-20 21:31:27 -0400

    imported middleware

---
+++ controllers/concerts.js
@@ -0,0 +1,15 @@
+const Concert = require("../models/schema");
+const express = require("express");
+const router = express.Router();
+
+//Create
+
+//Index
+
+//Show
+
+//Delete
+
+//Update
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const cors = require('cors')
+const concertRouter = require('./controllers/concerts')
+
+app.use(cors({origin: 'http://localhost:5173'}))
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.json());
+app.use(logger('dev'));
+
+//Routes
+
+app.use('/concerts', concertRouter)
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
